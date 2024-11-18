0ae7f2dea2ef0e7495b89f84724fa331b4747dbe
cachefulton <98287295+cachefulton@users.noreply.github.com>
2024-11-18 14:02:52 -0700

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST:'jenson-mysql.c8qwuofu4md8.us-east-1.rds.amazonaws.com',
-    USER:'kennyjay',
-    PASSWORD:'mysqluser',
+    HOST:'',
+    USER:;'',
+    PASSWORD:'',
     DB:'cf_teams_spring2023'
 }
